adf55bfcae354227b3cc512446e19a7c062ddfeb
Your Name01~git <saifsahai2002@gmail.com>
2025-08-11 19:59:00 +0600

    ..

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-    const userType = request.cookies.get('userType')?.value || "user"
+    const userType = request.cookies.get('userType')?.value || "admin"
 
     if (userType === "admin") {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    
     return NextResponse.redirect(new URL('/', request.url))
 }
 
