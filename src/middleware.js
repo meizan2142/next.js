747adbe669c224df255093cdeb0372ee7e0e73b3
Your Name01~git <saifsahai2002@gmail.com>
2025-08-11 14:53:35 +0600

    practising

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,15 @@
 import { NextResponse } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    return NextResponse.redirect(new URL('/products', request.url))
+    const userType = request.cookies.get('userType')?.value || "user"
+
+    if (userType === "admin") {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+    
+    return NextResponse.redirect(new URL('/', request.url))
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
     matcher: '/about',
 }
