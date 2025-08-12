f8efa64460ed993940a060206ab197d165bd3231
Your Name01~git <saifsahai2002@gmail.com>
2025-08-12 18:25:36 +0600

    practising..

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,6 +1,20 @@
-const SevicesPage = () => {
+import { getPosts } from "@/api/api"
+
+const SevicesPage = async () => {
+    const postData = await getPosts()
     return (
-        <div>SevicesPage</div>
+        <div className="grid grid-cols-4 gap-4">
+            <div>
+                {
+                    postData.map(({ id, body, title }) => {
+                        <div key={id} className="space-y-4 border text-black">
+                            <h1>Title: {title}</h1>
+                            <p>Description: {body}</p>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
