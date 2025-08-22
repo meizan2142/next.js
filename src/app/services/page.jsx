f657e2a3061f3907be79df8ad033ca9ea2113857
Your Name01~git <saifsahai2002@gmail.com>
2025-08-22 11:48:36 +0600

    working

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -8,9 +8,10 @@ const SevicesPage = async () => {
             <div className="grid grid-cols-4 gap-4 p-4">
                 {
                     postData.slice(0, 12).map(({ id, body, title }) => (
-                        <div key={id} className="space-y-4 border text-black p-2">
+                        <div key={id} className="space-y-4 border flex flex-col text-black p-2">
                             <h1>Title: {title}</h1>
                             <p>Description: {body}</p>
+                            <button className="mt-auto border p-2 rounded-2xl bg-amber-500 text-white">View Details</button>
                         </div>
                     ))
                 }
