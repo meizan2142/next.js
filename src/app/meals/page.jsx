1980f321876cb76541500222cbd45dab180d870f
Your Name01~git <saifsahai2002@gmail.com>
2025-08-29 22:33:52 +0600

    exploring meta data

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,5 +1,11 @@
 import Meals from "@/Components/Meals"
 
+
+export const metadata = {
+    title: "Meals | Next.js",
+    description: "Meals Page",
+};
+
 const MealsPage = () => {
     return (
         <div className="h-screen p-12">
