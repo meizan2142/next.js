0795f3ea3503bcc764d25e0cf4b28c54489ca794
Your Name01~git <saifsahai2002@gmail.com>
2025-08-30 20:09:46 +0600

    use Next Image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
+const imageLink = [
+  "https://ik.imagekit.io/Gamerskit/mask%20images/Defender%20Black/IMG-20250806-WA0011.jpg?updatedAt=1754488316618",
+  "https://ik.imagekit.io/Gamerskit/mask%20images/Defender%20Black/IMG-20250806-WA0012.jpg?updatedAt=1754488316574",
+  "https://ik.imagekit.io/Gamerskit/mask%20images/Defender%20Black/IMG-20250806-WA0013.jpg?updatedAt=1754488316570",
+  "https://ik.imagekit.io/Gamerskit/mask%20images/Defender%20Black/IMG-20250806-WA0015.jpg?updatedAt=1754488316387"
+]
+
 export default function Home() {
   return (
-    <div>
-      <h1>Next.js Lesson Begins</h1>
+    <div className="h-screen">
+      <div className="grid grid-cols-2">
+        {
+          imageLink.map((img) => <Image key={img}
+            src={img}
+            alt="Picture of the author"
+            width={800}
+            height={500}
+          />)
+        }
+      </div>
     </div>
   );
 }
