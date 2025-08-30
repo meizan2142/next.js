b50a629b2115ca87a08429ed05b916a48c7f539b
Your Name01~git <saifsahai2002@gmail.com>
2025-08-30 19:49:00 +0600

    set the dynamic metadata

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,5 +1,19 @@
 import { getSinglePostDetails } from "@/api/api";
 
+export const generateMetadata = async ({ params }) => {
+    const post = await getSinglePostDetails(params.id)
+
+    return {
+        title: `${post.title} | Saif`,
+        description: post.body,
+    }
+}
+
+// export const metadata = {
+//     title: post.title,
+//     description: post.body,
+// };
+
 const SinglePost = async ({ params }) => {
     const { body, title } = await getSinglePostDetails(params.id)
 
