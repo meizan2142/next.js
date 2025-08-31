fcf9511a219949e3a776b89c63a16907226df212
Your Name01~git <saifsahai2002@gmail.com>
2025-08-31 20:22:39 +0600

    successfully deployed on vercel

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,6 +1,9 @@
 const CategoriesPage = () => {
     return (
-        <div className="p-12 text-center font-bold text-4xl h-screen">Categories Page with Next.js</div>
+        <div className="p-12 text-center h-screen">
+            <h1 className="font-bold text-4xl">Categories Page with Next.js</h1>
+            <p>Uno, Dos, Tres Hala Madrid</p>
+        </div>
     )
 }
 
