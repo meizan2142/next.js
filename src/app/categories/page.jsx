e92a56fa204a5f893249f78d19706f5f117a3244
Your Name01~git <saifsahai2002@gmail.com>
2025-08-31 19:33:23 +0600

    ..

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,6 +1,6 @@
 const CategoriesPage = () => {
     return (
-        <div className="p-12 text-center font-bold text-4xl">Categories Page with Next.js</div>
+        <div className="p-12 text-center font-bold text-4xl h-screen">Categories Page with Next.js</div>
     )
 }
 
