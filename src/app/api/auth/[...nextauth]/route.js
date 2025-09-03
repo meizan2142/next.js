19baacb94196bc753dd26e3b40d9c2b5faeb2f41
Your Name01~git <saifsahai2002@gmail.com>
2025-09-03 23:49:53 +0600

    if user matched then it returns a token

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+const users = [
+    {
+        "id": 1,
+        "name": "Alice Johnson",
+        "email": "alice.johnson@example.com",
+        "password": "Alice@123"
+    },
+    {
+        "id": 2,
+        "name": "Brian Smith",
+        "email": "brian.smith@example.com",
+        "password": "BrianPass!456"
+    },
+    {
+        "id": 3,
+        "name": "Clara Williams",
+        "email": "clara.williams@example.com",
+        "password": "Clara#789"
+    }
+]
+
+
 // http://localhost:3000/api/auth/signin
 
 const handler = NextAuth({
@@ -8,8 +30,8 @@ const handler = NextAuth({
         strategy: "jwt"
     },
     providers: [
-        CredentialsProvider ({
-            credentials : {
+        CredentialsProvider({
+            credentials: {
                 email: {
                     label: "Email",
                     type: "email",
@@ -25,13 +47,19 @@ const handler = NextAuth({
             },
 
             async authorize(creadentials) {
-                if(!creadentials) {
+                const { email, password } = creadentials;
+                if (!creadentials) {
                     return null;
                 }
-                return true;
+                const currentUser = users.find(user => user.email === email);
+                if (currentUser && currentUser.password === password) {
+                    return currentUser;
+                }
+                return null;
             }
         })
     ]
 })
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
+
